072a474294294b7dce6b93473e14fcab183c06e6
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-04-14 09:18:36 -0500

    isograms

--- codewars/scripts/main.js
+++ codewars/scripts/main.js
@@ -1,44 +1,44 @@
-// Count the number of Duplicates
-// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+// Isograms
 
-// Example
-// "abcde" -> 0 # no characters repeats more than once
-// "aabbcde" -> 2 # 'a' and 'b'
-// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
-// "indivisibility" -> 1 # 'i' occurs six times
-// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
-// "aA11" -> 2 # 'a' and '1'
-// "ABBA" -> 2 # 'A' and 'B' each occur twice
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
 
-//  My Solution
+// isIsogram("Dermatoglyphics") == true
+// isIsogram("aba") == false
+// isIsogram("moOse") == false // -- ignore letter case
 
-function duplicateCount(text) {
-  const chars = text.split('').toLowerCase()
+// My Solution
+
+function isIsogram(str) {
+  if (!str) {
+    return true
+  }
+  const chars = str.toLowerCase().split('')
   return [
     ...new Set(
       chars.filter((c, index) => {
-        return text.indexOf(c) !== index
+        return chars.indexOf(c) !== index
       })
     ),
-  ].length
+  ].length > 0
+    ? false
+    : true
 }
 
-console.log(duplicateCount('Indivisibility'))
-console.log(duplicateCount('Indivisibilities'))
+console.log(isIsogram('Dermatoglyphics'))
+console.log(isIsogram('Dermatoglyphicss'))
+console.log(isIsogram('Dermatoglyphicsz'))
+console.log(isIsogram(''))
 
 // Other Solutions
 
-function duplicateCount(text) {
-  return (
-    text
-      .toLowerCase()
-      .split('')
-      .sort()
-      .join('')
-      .match(/([^])\1+/g) || []
-  ).length
+function isIsogram(str) {
+  return !str.match(/([a-z]).*\1/i)
 }
 
-function duplicateCount(text) {
-  return new Set(text.toLowerCase().match(/(.)(?=.*\1)/gi)).size
+function isIsogram(str) {
+  return !/(\w).*\1/i.test(str)
+}
+
+function isIsogram(str) {
+  return !str || str.length === new Set(str.toLowerCase()).size
 }
