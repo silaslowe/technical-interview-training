5dd9163e92a4d948e2f30aed67d4291794e1effe
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-04-20 15:17:31 -0500

    Word to Binary

--- codewars/scripts/main.js
+++ codewars/scripts/main.js
@@ -1,34 +1,21 @@
-// Numbers To Letters
+// Word to binary
 
-// Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+// Write a function that takes a string and returns an array containing binary numbers equivalent to the ASCII codes of the characters of the string. The binary strings should be eight digits long.
 
-// All inputs will be valid.
+// Example: 'man' should return [ '01101101', '01100001', '01101110' ]
 
-// My Solution
+function wordToBin(str){
+    return str.split("").map(c => c.charCodeAt(0).toString(2).padStart(8, '0'))
+  }
 
-function switcher(x) {
-  const arr = []
-  for(let i = 0; i < x.length; i ++) {
-    if(x[i] >= 1 & x[i] <= 26) {
-      arr.push(String.fromCharCode(97 + (26 - x[i])))
-    } else if (x[i] === '27') {
-    arr.push("!")
-    } else if (x[i] === '28') {
-    arr.push("?")
-    } else {
-    arr.push(String.fromCharCode(32))
-    }
-}
-  return arr.join("")
-}
-
-console.log(switcher(['24', '27', '12', '23', '22', '4', '26', '9', '28','8']))
+  console.log(wordToBin("man"))
+  console.log(wordToBin("wecking"))
 
 // Other Solutions
 
-const alpha = ' zyxwvutsrqponmlkjihgfedcba!? '
-const switcher = x => x.map(item => alpha[item]).join('')
-
-const switcher=x=>x.reduce((a,b)=>a+" ?!abcdefghijklmnopqrstuvwxyz"[29-b],"")
+const wordToBin = (x) => [...x].map(el=> el.codePointAt().toString(2).padStart(8,'0'))
 
 
+function wordToBin(str) {
+    return Array.from(str, c => (256|c.charCodeAt(0)).toString(2).slice(1));
+  }
