0807d7d5cde1bf7c6f2a6e7e5c5e3c3589cda1d1
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-05-14 14:27:20 -0500

    hacker rank arrays

--- hackerrank/scripts/main.js
+++ hackerrank/scripts/main.js
@@ -1,33 +1,8 @@
-let grades = [73, 67, 38, 33]
-
-// const grader = (grades) => {
-//   grades.map((g) => {
-//     if (g < 38) {
-//       console.log(g)
-//     } else if ((g + 2) % 5 === 0) {
-//       console.log(g + 2)
-//     } else if ((g + 1) % 5 === 0) {
-//       console.log(g + 1)
-//     } else {
-//       console.log(g)
-//     }
-//   })
-// }
-
-const grader = (grades) => {
-  let final = []
-  for (let i = 0; i < grades.length; i++) {
-    if (grades[i] < 38) {
-      final.push(grades[i])
-    } else if ((grades[i] + 2) % 5 === 0) {
-      final.push(grades[i] + 2)
-    } else if ((grades[i] + 1) % 5 === 0) {
-      final.push(grades[i] + 1)
-    } else {
-      final.push(grades[i])
-    }
-  }
-  return final
+function getSecondLargest(nums) {
+   const arr = [...new Set(nums)].sort((a, b) => a -b)
+   console.log(arr[arr.length-1])
+    return arr[arr.length-2]
 }
 
-console.log(grader(grades))
+console.log(getSecondLargest([2,3,6,6,5]))
+console.log(getSecondLargest([10, 9, 9, 8, 8, 11, 8, 0, 9, 1]))
