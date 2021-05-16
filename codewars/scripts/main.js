3a5560d46ec450888bc0050f33c1dbd519982fde
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-05-15 21:57:56 -0500

    two biggest in array

--- codewars/scripts/main.js
+++ codewars/scripts/main.js
@@ -1,28 +1,7 @@
-console.log("Hello")
+function largestPairSum(numbers){
+    const num = numbers.sort((a,b) => b-a)
+    return num[0] + num[1]
+}
 
-// My Solution:
-
-const maxCoins = (piles) => {
-    let pilesCopy = [...piles].sort((a,b) => b - a)
-    let largest = []
-    pilesCopy.splice((pilesCopy.length/3)*2, pilesCopy.length)
-    pilesCopy.map((num, i) => {
-        if(i % 2 !== 0){
-          return largest.push(num)
-        } 
-    })
-    return largest.reduce((a,b) => a+b,0)
-};
-
-// var maxCoins = function(piles) {
-//     piles.sort((a, b) => a - b)
-//     let result = 0
-//     for (let i = 1; i <= (piles.length)/3; i++) {
-//         console.log(result, i, )
-//         result += piles[piles.length - 2*i]
-//     }
-//     return result
-// };
-
-console.log(maxCoins([9,8,7,6,5,1,2,3,4]))
-console.log(maxCoins([2,4,1,2,7,8]))
+console.log(largestPairSum([10,14,2,23,19]))
+console.log(largestPairSum([-100,-29,-24,-19,19]))
