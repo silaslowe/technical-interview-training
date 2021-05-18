3e0f9cf8f69983c1cdd5276f2f358cf02334bad0
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-05-18 11:48:46 -0500

    Ice cream

--- codewars/scripts/main.js
+++
@@ -1,9 +0,0 @@
-function convert(s) {
-    let counter = 0
-    const hash = new Map()
-    const arr =  [...new Set(s.split(""))]
-    arr.map(letter => {hash.set(letter, counter)})
-  }
-
-console.log(convert("CodeWars"))
-console.log(convert("KATA"))
